586e68b5c4c4cbf64f4bced0bb2f834e0bfc367e
FabriceG <fabrice31toulouse@gmail.com>
2016-05-10 00:14:44 +0200

    MAJ des méthodes de la Classe "generique" MaTableDAO

--- projet_fil_rouge/02-JAVA_SE/projet_fil_rouge.MVC/src/packageDAO/MaTableDAO.java
+++ projet_fil_rouge/02-JAVA_SE/projet_fil_rouge.MVC/src/packageDAO/MaTableDAO.java
@@ -1,6 +1,7 @@
 package packageDAO;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,21 +16,25 @@ public class MaTableDAO
 	private Integer id;
 	private String nom = "nom_test";
 	private LocalDate date;
-	
+	private static final String table = "Adresse";
 
-	boolean dbUpdate = true;
+	//************************************************************************
+	//** CONSTRUCTORS
+	//************************************************************************
+
+	public MaTableDAO(String nom, LocalDate date) {
+		this.setNom(nom);
+		this.setDate(date);
+	}
 	
-	boolean dbInsert = true;
-	
-	static boolean dbDeleteFromId;
+	public MaTableDAO(Integer id, String nom, LocalDate date) {
+		this(nom,date);
+		this.setId(id);
+	}
 	
 	//************************************************************************
 	//** GETTERS SETTERS
 	//************************************************************************
-	
-	public MaTableDAO(int int1, String string, LocalDate localDate) {
-		// TODO Auto-generated constructor stub
-	}
 
 	public Integer getId() {
 		return id;
@@ -60,24 +65,26 @@ public class MaTableDAO
 	//************************************************************************
 	
 	/**
-	 * 
+	 * Description of the method dbSelectById
 	 * @param id
-	 * @return
+	 * @return MaTableDAO
 	 */
 
 	public MaTableDAO dbSelectById(int id){
 
 		MaTableDAO retObj = null;
 		Connection conn = JDBCConnection.getInstance("","");
-		String sql = "SELECT * FROM maTable WHERE id="+id;
+		String sql = "SELECT * FROM "+ table + "WHERE id="+id;
 		Statement stmt = null;
+		ResultSet rs = null;
+		
 		try {
 			stmt = conn.prepareCall(sql);
 			// ResultSet est une collection
-			ResultSet rs = stmt.executeQuery(sql);
+			rs = stmt.executeQuery(sql);
 			while(rs.next())
 			{
-				MaTableDAO obj = new MaTableDAO (rs.getInt("Id"), 
+				MaTableDAO obj = new MaTableDAO (rs.getInt("id"), 
 												rs.getString("nom"),
 												rs.getDate("date").toLocalDate());
 			}
@@ -85,24 +92,30 @@ public class MaTableDAO
 			stmt.close();
 		} 
 		catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return retObj;
 	}
 	
+	/**
+	 * Description of the method dbSelectAll
+	 * @return HashSet <MaTableDAO> 
+	 */
 	public HashSet <MaTableDAO> dbSelectAll()
 	{
 		HashSet <MaTableDAO> retObj = new HashSet<MaTableDAO>();
 		Connection conn = JDBCConnection.getInstance();
-		String sql = "SELECT * FROM maTable ORDER BY id ASC";
+		String sql = "SELECT * FROM "+ table + " ORDER BY id ASC";
 		Statement stmt = null;
+		ResultSet rs = null;
 		try {
 			stmt = conn.prepareCall(sql);
 			// ResultSet est une collection
-			ResultSet rs = stmt.executeQuery(sql);
+			rs = stmt.executeQuery(sql);
 			while(rs.next())
 			{
-				MaTableDAO obj = new MaTableDAO (rs.getInt("Id"), 
+				MaTableDAO obj = new MaTableDAO (rs.getInt("id"), 
 												rs.getString("nom"),
 												rs.getDate("date").toLocalDate());
 				retObj.add(obj);
@@ -111,15 +124,16 @@ public class MaTableDAO
 			stmt.close();
 		} 
 		catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return retObj;
 	}
 	/**
+	 * Description of the method dbInsert
+	 * @return boolean
 	 * 
-	 * @return
-	 * 
-	 * @ TODO pour éviter des injections dans les requêtes, on spécifie le retour attendu dans ?
+	 * - pour éviter des injections dans les requêtes, on spécifie le retour attendu dans ?
 	 * 
 	 */
 	public boolean dbInsert(){
@@ -143,19 +157,125 @@ public class MaTableDAO
 		String sql = "INSERT INTO maTable"
 					+ "(nom, date) VALUES (?,?)";
 		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		try {
 			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(0, this.getNom());
+			pstmt.setDate(1, Date.valueOf(this.getDate()));
+			
 			int nb = pstmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
-			ret = true;
-			ResultSet rs = pstmt.getGeneratedKeys();
-			rs.first();
-			this.dbSelectById(rs.getInt(1));
+			if (1 == nb)
+			{
+				ret = true;
+				rs = pstmt.getGeneratedKeys();
+				if (rs.first())
+				{
+					this.setId(rs.getInt(1));
+				}
+			}
 			pstmt.close();
 		} 
 		
 		catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return ret;
 	}
+	
+	/**
+	 * Description of the method dbUpdateFromId.
+	 * @param id 
+	 * @return boolean
+	 */
+	public boolean dbUpdate() {
+		// FIXME Why rowcount if it just 1 ?
+		int updateRowCount = 0;
+
+		Connection conn = JDBCConnection.getInstance();
+		String sql = "UPDATE "+table+" SET nom=?, date=? WHERE id="+getId();
+
+		PreparedStatement stmt = null;
+		
+		try {
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, this.getNom());
+			stmt.setDate(2, Date.valueOf(this.getDate()));
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return (1 == updateRowCount);
+	}
+	/**
+	 * Description of the method hasSameContent
+	 * @param obj
+	 * @return boolean
+	 */
+	public boolean hasSameContent(MaTableDAO obj){
+		return(this.nom.equals(obj.getNom())
+				&& this.date.equals(obj.getDate()));
+	}
+
+	/**
+	 * Description of the method dbExistFromId.
+	 * @param id
+	 * @return
+	 * 
+	 * @NOTE - Identical method for all classes !
+	 */
+	public static boolean dbExistFromId(Integer id) {
+		boolean retBool = false; 
+		Connection conn = JDBCConnection.getInstance();
+		
+		String sql = "SELECT * FROM "+table+" WHERE id="+id;
+		ResultSet rs = null;
+		Statement stmt = null;
+		
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			
+			if (rs.first()){
+				retBool = true;
+			}
+			rs.close();
+			stmt.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return retBool;
+	}
+
+	/**
+	 * Description of the method dbDeleteFromId.
+	 * @param id 
+	 * @return boolean
+	 * 
+	 * @NOTE - Identical method for all classes !
+	 */
+	public static boolean dbDeleteFromId(int id) {
+		boolean dbDeleteFromId = false;
+		
+		Connection conn = JDBCConnection.getInstance();
+		
+		String sql = "DELETE FROM "+table+" WHERE id="+id;
+		Statement stmt = null;
+		
+		try {
+			stmt = conn.createStatement();
+			dbDeleteFromId = (1 == stmt.executeUpdate(sql));
+			stmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dbDeleteFromId;
+	}
+	
+	
 }
